11fe13c095d3779e5662d3e5a5badbb8b35bd854
Rakesh07778777 <bossrakesh43@gmail.com>
2026-07-06 21:38:55 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main>
+      <h1 className="text-5xl font-robert-medium font-bold">
+        Tailwind is Working 🚀
+      </h1>
+    </main>
+  )
+}
+
+export default App;
