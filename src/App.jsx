84b7a9b9d1ec5ac120f9aceba33e52ce39550638
Rakesh07778777 <bossrakesh43@gmail.com>
2026-07-06 22:53:12 +0530

    color and fonts

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,18 @@ import React from 'react'
 const App = () => {
   return (
     <main>
-      <h1 className="text-5xl font-robert-medium font-bold">
+      <h1 className="text-5xl text-blue-300 font-circular-web font-bold">
         Tailwind is Working 🚀
       </h1>
+      <h1 className="font-zentry">Zentry</h1>
+
+<p className="font-general text-violet-300">Robert Regular</p>
+
+<p className="font-circular-web">Robert Regular</p>
+
+<p className="font-robert-medium">Robert Regular</p>
+
+<p className="font-robert-regular">Robert Regular</p>
     </main>
   )
 }
