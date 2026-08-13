aa30f5833066f6ecbae781dce8832dc7164b60c5
Rakesh07778777 <bossrakesh43@gmail.com>
2026-08-13 23:52:38 +0530

    hero with tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,10 @@
-import React from 'react'
+
+import Hero from './components/Hero';
 
 const App = () => {
   return (
-    <main>
-      <h1 className="text-5xl text-blue-300 font-circular-web font-bold">
-        Tailwind is Working 🚀
-      </h1>
-      <h1 className="font-zentry">Zentry</h1>
-
-<p className="font-general text-violet-300">Robert Regular</p>
-
-<p className="font-circular-web">Robert Regular</p>
-
-<p className="font-robert-medium">Robert Regular</p>
-
-<p className="font-robert-regular">Robert Regular</p>
+    <main className = "relative min-h-screen w-screen overflow-x-hidden">
+      <Hero/>
     </main>
   )
 }
