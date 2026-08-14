fe11a40216b93ac95429623395cb4a4b3514ecbb
Rakesh07778777 <bossrakesh43@gmail.com>
2026-08-15 01:12:13 +0530

    video loop

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,23 +3,45 @@ import { useRef, useState } from 'react';
 const [currentIndex, setCurrentIndex] = useState(1);
 const [hasClicked, setHasClicked] = useState(false); 
 const [isLoading, setIsLoading] = useState(true);
-const [loadedVideos, setLoadedVideos] = ueState(0); 
+const [loadedVideos, setLoadedVideos] = useState(0); 
 
-const totalVideos = 4;
+const totalVideos = 3;
 
 const nextVideoRef = useRef(null);
 
+const handleVideoLoad = () => {
+  setLoadedVideos((prev) => prev + 1);
+}
+
+const upComingVideoIndex = (currentIndex % totalVideos) + 1;
+
   const handleMiniVdClick = () => {
+
     setHasClicked (true);
-    currentIndex((prevIndex) => prevIndex + 1);
+
+    setCurrentIndex(upComingVideoIndex);
+
   }
+
+
+  const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
+
    return (
      <div className="relative h-dvh w-screen overflow-x-hidden">
       <div id="video-frame" className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75">
           <div>
             <div className="mask-clip-path  absolute absolute-center z-50 size-64 cursor-pointer rounded-lg overflow-hidden">
-                <div className="">
-                  miniVideoPlayer
+                <div onClick={handleMiniVdClick} className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100">
+                  <video
+                    className="origin-center size-64 scale-150 object-cover object-center"
+                    ref={nextVideoRef}
+                    src={getVideoSrc(currentIndex + 1)}
+                    loop
+                    muted
+                    id='current-video'
+
+                    onLoadedData={handleVideoLoad}
+                  />
                 </div>
             </div>
           </div>
