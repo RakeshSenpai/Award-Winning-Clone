13435cc7937dcd686b05254444ddfbbcdebfd754
Rakesh07778777 <bossrakesh43@gmail.com>
2026-09-08 23:45:08 +0530

    video loaded

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -80,6 +80,18 @@ useGSAP(() => {
 
    return (
      <div className="relative h-dvh w-screen overflow-x-hidden">
+
+      {isLoading && (
+        <div>
+          <div className='three-body'>
+            <div className='three-body__dot'/>
+            <div className='three-body__dot'/>
+            <div className='three-body__dot'/>
+          </div>
+        </div>
+
+            )}
+
       <div id="video-frame" className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75">
           <div>
             <div className="mask-clip-path  absolute absolute-center z-50 size-64 cursor-pointer rounded-lg overflow-hidden">
