ad189e7af4177655f87eab75944153e080ac9c02
Rakesh07778777 <bossrakesh43@gmail.com>
2026-09-08 23:38:22 +0530

    gsap scroll trigger

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -58,7 +58,20 @@ useGSAP(() => {
 
 useGSAP(() => {
   gsap.set('#video-frame', {
-    clipPath: 'polygon(20% 0%, 80% 0%, 100% 100%, 0% 100%)',
+    clipPath: 'polygon(14% 0%, 72% 0%, 90% 90%, 0% 100%)',
+    borderRadius: '0 0 40% 10%'
+  })
+
+  gsap.from('#video-frame', {
+    clipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)',
+    borderRadius: '0 0 0 0',
+    ease: 'power1.inOut',
+    scrollTrigger: {
+       trigger: '#video-frame',
+       start: 'center center',
+       end: 'bottom center',
+       scrub: true
+    }
   })
 })
 
